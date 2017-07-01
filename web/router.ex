72893cb0f1d82c4f1f5cd841cbf52bfeeb13faa9
Kyle Mellander <kmellander@focusvision.com>
2017-07-01 09:44:50 -0700

    Remove browser from server

--- web/router.ex
+++ web/router.ex
@@ -1,14 +1,6 @@
 defmodule NoegenApi.Router do
   use NoegenApi.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
@@ -17,12 +9,6 @@ defmodule NoegenApi.Router do
     plug Guardian.Plug.VerifyHeader, realm: "Bearer"
     plug Guardian.Plug.LoadResource
     plug NoegenApi.Web.AuthenticateUser
-  end
-
-  scope "/", NoegenApi do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
   end
 
   scope "/api" do
