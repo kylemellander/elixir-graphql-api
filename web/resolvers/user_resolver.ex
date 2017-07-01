78b917ce532890e6e1b31d8aa4741eeb8379468b
Kyle Mellander <kmellander@focusvision.com>
2017-07-01 09:38:22 -0700

    Add find method to user

--- web/resolvers/user_resolver.ex
+++ web/resolvers/user_resolver.ex
@@ -5,4 +5,15 @@ defmodule NoegenApi.UserResolver do
   def all(_args, _info) do
     {:ok, Repo.all(User)}
   end
+
+  def find(%{id: id}, _info) do
+    find_user(id)
+  end
+
+  defp find_user(id) do
+    case Repo.get(User, id) do
+      nil -> {:error, "Not Found"}
+      user -> {:ok, user}
+    end
+  end
 end
