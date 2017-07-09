4b507fd46d46d2890760951dea1c7cf2be9f67c6
Kyle Mellander <kmellander@focusvision.com>
2017-07-09 15:41:34 -0700

    Add login resolver

--- web/resolvers/user_resolver.ex
+++ web/resolvers/user_resolver.ex
@@ -18,14 +18,19 @@ defmodule NoegenApi.UserResolver do
     find_user(id)
   end
 
-  def current(_args, _info) do
-    {:error, "Not Found"}
+  def create(args, _info) do
+    changeset = register_changeset(args)
+    case Repo.insert(changeset) do
+      {:error, changeset} -> {:error, format_errors(changeset.errors)}
+      {:ok, user} -> {:ok, user}
+    end
   end
 
-  def create(args, _info) do
-    %User{}
-      |> User.registration_changeset(args)
-      |> Repo.insert
+  def login(args, _info) do
+    with {:ok, user} <- NoegenApi.Session.authenticate(args, Repo),
+      {:ok, jwt, _ } <- Guardian.encode_and_sign(user, :access) do
+      {:ok, %{token: jwt}}
+    end
   end
 
   defp find_user(id) do
@@ -33,5 +38,13 @@ defmodule NoegenApi.UserResolver do
       nil -> {:error, "Not Found"}
       user -> {:ok, user}
     end
+  end
+
+  defp register_changeset(args) do
+    %User{} |> User.registration_changeset(args)
+  end
+
+  defp format_errors(errors) do
+    Enum.map(errors, fn({key, {message, _}}) -> "#{key} #{message}" end)
   end
 end
